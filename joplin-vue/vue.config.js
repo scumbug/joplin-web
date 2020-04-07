b85d3d46992e4f41f8455c1427f8138ad65281c6
FoxMaSk <foxmask@protonmail.com>
2020-04-08 00:14:01 +0200

    Joplin web trying to access 'undefined' url path #54 fixed

--- joplin-vue/vue.config.js
+++ joplin-vue/vue.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   publicPath: process.env.NODE_ENV !== 'production'
-    ? '/static/'
-    : process.env.JW_BASE_URL + '/static',
+    ? '/'
+    : 'http://0.0.0.0:8001' + process.env.JW_BASE_URL + 'static',
   outputDir: '../joplin_web/static',
   indexPath: '../templates/index.html',
   filenameHashing: false,
